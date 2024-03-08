513e9088252967a561f97b569277dbd3a83215a7
kura1aym <mukhtarkuralay249@mail.ru>
2024-03-08 21:33:21 +0500

    add everything function

--- newsapi/src/main/java/dev/androidbroadcast/newsapi/NewsApi.kt
+++ newsapi/src/main/java/dev/androidbroadcast/newsapi/NewsApi.kt
@@ -1,4 +1,13 @@
 package dev.androidbroadcast.newsapi
 
-class NewsApi {
+interface NewsApi {
+
+    @GET("/everything")
+    fun everything(
+        @Query("q") query: String? = null,
+        @Query("from") date: Date? = null,
+        @Query("to") to: Date? = null,
+        @Query("to") languages: String? = null,
+        @Query("sortBy") sortBy: SortBy? = null,
+    )
 }
