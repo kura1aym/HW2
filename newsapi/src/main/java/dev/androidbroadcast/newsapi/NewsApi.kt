73fad7f6d557d4344ee855c66b4f594d895b4d77
kura1aym <mukhtarkuralay249@mail.ru>
2024-03-09 01:57:15 +0500

    init API

--- newsapi/src/main/java/dev/androidbroadcast/newsapi/NewsApi.kt
+++ newsapi/src/main/java/dev/androidbroadcast/newsapi/NewsApi.kt
@@ -1,10 +1,16 @@
 package dev.androidbroadcast.newsapi
 
 import androidx.annotation.IntRange
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dev.androidbroadcast.newsapi.modules.Article
 import dev.androidbroadcast.newsapi.modules.Response
+import dev.androidbroadcast.newsapi.modules.Language
 import dev.androidbroadcast.newsapi.modules.SortBy
-import org.intellij.lang.annotations.Language
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.create
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.Date
@@ -16,10 +22,32 @@ interface NewsApi {
         @Query("q") query: String? = null,
         @Query("from") date: Date? = null,
         @Query("to") to: Date? = null,
-        @Query("to") languages: List<Language>? = null,
+        @Query("languages") languages: List<Language>? = null,
         @Query("sortBy") sortBy: SortBy? = null,
         @Query("pageSize") @IntRange(from=0, to=100) pageSize: Int = 100,
         @Query("page") @IntRange(from=1)  page: Int = 1,
     ): Response<Article>
 }
 
+fun NewsApi(
+    baseUrl: String,
+    okHttpClient: OkHttpClient? = null,
+    json: Json = Json,
+): NewsApi {
+    return retrofit(baseUrl, okHttpClient, json).create()
+}
+
+private fun retrofit(
+    baseUrl: String,
+    okHttpClient: OkHttpClient?,
+    json: Json,
+): Retrofit {
+    val jsonConverterFactory = json.asConverterFactory(MediaType.get("application/json"))
+    return Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(jsonConverterFactory)
+        .run {
+            if (okHttpClient != null) client(okHttpClient) else this
+        }
+        .build()
+}
