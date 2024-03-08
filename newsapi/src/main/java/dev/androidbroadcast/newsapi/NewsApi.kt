ea17a9c410b91369c1a8bf77f7a42e0270ee52d6
kura1aym <mukhtarkuralay249@mail.ru>
2024-03-09 00:03:59 +0500

    add module Source

--- newsapi/src/main/java/dev/androidbroadcast/newsapi/NewsApi.kt
+++ newsapi/src/main/java/dev/androidbroadcast/newsapi/NewsApi.kt
@@ -1,17 +1,18 @@
 package dev.androidbroadcast.newsapi
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import androidx.annotation.IntRange
+import dev.androidbroadcast.newsapi.modules.Article
+import dev.androidbroadcast.newsapi.modules.Response
+import dev.androidbroadcast.newsapi.modules.SortBy
 import org.intellij.lang.annotations.Language
 import retrofit2.http.GET
 import retrofit2.http.Query
-import androidx.annotation.IntRange
 import java.util.Date
 
 interface NewsApi {
 
     @GET("/everything")
-    fun everything(
+    suspend fun everything(
         @Query("q") query: String? = null,
         @Query("from") date: Date? = null,
         @Query("to") to: Date? = null,
@@ -19,62 +20,6 @@ interface NewsApi {
         @Query("sortBy") sortBy: SortBy? = null,
         @Query("pageSize") @IntRange(from=0, to=100) pageSize: Int = 100,
         @Query("page") @IntRange(from=1)  page: Int = 1,
-    )
+    ): Response<Article>
 }
 
-@kotlinx.serialization.Serializable
-enum class language {
-    @SerialName("ar")
-    AR,
-
-    @SerialName("de")
-    DE,
-
-    @SerialName("en")
-    EN,
-
-    @SerialName("es")
-    ES,
-
-    @SerialName("fr")
-    FR,
-
-    @SerialName("he")
-    HE,
-
-    @SerialName("it")
-    IT,
-
-    @SerialName("nl")
-    NL,
-
-    @SerialName("no")
-    NO,
-
-    @SerialName("pt")
-    PT,
-
-    @SerialName("ru")
-    RU,
-
-    @SerialName("sv")
-    SV,
-
-    @SerialName("ud")
-    UD,
-
-    @SerialName("zh")
-    ZH,
-}
-
-@Serializable
-enum class SortBy {
-    @SerialName("relevancy")
-    RELEVANCY,
-
-    @SerialName("popularity")
-    POPULARITY,
-
-    @SerialName("publishedAt")
-    PUBLISHED_AT,
-}
---
+++ newsapi/src/main/java/dev/androidbroadcast/newsapi/modules/Source.kt
@@ -0,0 +1,10 @@
+package dev.androidbroadcast.newsapi.modules
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Source (
+    @SerialName("id") val id: String,
+    @SerialName("name") val name: String
+)
