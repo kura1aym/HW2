89ab02a9f4342e5df9176b3912caf9d7ad5773df
kura1aym <mukhtarkuralay249@mail.ru>
2024-03-09 00:02:55 +0500

    add module Article

---
+++ newsapi/src/main/java/dev/androidbroadcast/newsapi/modules/Article.kt
@@ -0,0 +1,17 @@
+package dev.androidbroadcast.newsapi.modules
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.util.Date
+
+@Serializable
+data class Article (
+    @SerialName("source") val source: Source,
+    @SerialName("author") val author: String,
+    @SerialName("title") val title: String,
+    @SerialName("description") val description: String,
+    @SerialName("url") val url: String,
+    @SerialName("urlToImage") val urlToImage: String,
+    @SerialName("publishedAt") val publishedAt: Date,
+    @SerialName("content") val content: String,
+    )
