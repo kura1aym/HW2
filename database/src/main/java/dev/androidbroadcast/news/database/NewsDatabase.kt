32b5b4dc078d3aa274294b66a2a80713b1f5a57a
kura1aym <mukhtarkuralay249@mail.ru>
2024-03-10 01:20:10 +0500

    finishing database

--- database/src/main/java/dev/androidbroadcast/news/database/NewsDatabase.kt
+++ database/src/main/java/dev/androidbroadcast/news/database/NewsDatabase.kt
@@ -1,4 +1,21 @@
 package dev.androidbroadcast.news.database
 
-class NewsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import dev.androidbroadcast.news.database.dao.ArticleDao
+import dev.androidbroadcast.news.database.models.ArticleDBO
+
+@Database(entities = [ArticleDBO::class], version = 1)
+abstract class NewsDatabase : RoomDatabase() {
+    abstract fun articlesDao(): ArticleDao
+}
+
+fun NewsDatabase(applicationContext: Context): NewsDatabase {
+    return Room.databaseBuilder(
+        checkNotNull(applicationContext.applicationContext),
+        NewsDatabase::class.java,
+        "news"
+    ).build()
 }
---
+++ database/src/main/java/dev/androidbroadcast/news/database/utils/Conventers.kt
@@ -0,0 +1,4 @@
+package dev.androidbroadcast.news.database.utils
+
+class Conventers {
+}
