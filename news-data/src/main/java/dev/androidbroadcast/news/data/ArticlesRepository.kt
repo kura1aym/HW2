d5524c7f6c58e0313cf768ee79fb879b5d673a84
kura1aym <mukhtarkuralay249@mail.ru>
2024-03-15 21:27:37 +0500

    RequestResult mapper

--- news-data/src/main/java/dev/androidbroadcast/news/data/ArticlesRepository.kt
+++ news-data/src/main/java/dev/androidbroadcast/news/data/ArticlesRepository.kt
@@ -33,23 +33,24 @@ class ArticlesRepository(
         }
     }
 
-    private fun getAllFormServer() = flow { emit(api.everything()) }
-        .map { result ->
-            if (result.isSuccess) {
-                val response: ResponseDTO<ArticleDTO> = result.getOrThrow()
-                RequestResult.Success(response.articles)
-            } else {
-                RequestResult.Error(null)
-            }
-        }
-        .filterIsInstance<RequestResult.Success<List<ArticleDTO>?>>()
-        .map {  requestResult: RequestResult.Success<List<ArticleDTO>?> ->
-            requestResult.requireData()
-                .map { articleDto -> articleDto.toArticleDbo()}
-                .let {RequestResult.Success<List<ArticleDBO>?>(it)}
-        }.onEach { requestResult ->
-            database.articlesDao.insert(requestResult.requireData())
-        }
+    private fun getAllFormServer(): Flow<RequestResult<List<ArticleDBO>>> {
+      return flow { emit(api.everything()) }
+          .map { result ->
+              if (result.isSuccess) {
+                  val response: ResponseDTO<ArticleDTO> = result.getOrThrow()
+                  RequestResult.Success(response.articles)
+              } else {
+                  RequestResult.Error(null)
+              }
+          }
+          .filterIsInstance<RequestResult.Success<List<ArticleDTO>>>()
+          .map {  requestResult: RequestResult.Success<List<ArticleDTO>> ->
+              requestResult.map {dtos -> dtos.map { articleDto -> articleDto.toArticleDbo()  }}
+          }.onEach { requestResult: RequestResult<List<ArticleDBO>> ->
+              database.articlesDao.insert(requestResult.data)
+          }
+    }
+
 
     private fun gelAllFromDatabase(): Flow<RequestResult.Success<List<ArticleDBO>>> {
         return database.articlesDao
@@ -63,20 +64,19 @@ class ArticlesRepository(
     }
 }
 
-//with(requestResult.data) {
-//    if(requestResult.data != null) {
-//        requestResult.data.map { articleDto -> articleDto.toArticleDbo()}
-//    } else {
-//        null
-//    }
-//}.let {RequestResult.Success<List<ArticleDBO>?>(it)}
-//
-//checkNotNull(requestResult.data).map { articleDto -> articleDto.toArticleDbo()}
-
-sealed class RequestResult<E>(internal val data: E?) {
-    class InProgress<E>(data: E?) : RequestResult<E>(data)
-    class Success<E>(data: E?) : RequestResult<E>(data)
-    class Error<E>(data: E?) : RequestResult<E>(data)
+sealed class RequestResult<E>(internal val data: E) {
+    class InProgress<E>(data: E) : RequestResult<E>(data)
+    class Success<E>(data: E) : RequestResult<E>(data)
+    class Error<E>(data: E) : RequestResult<E>(data)
 }
 
 internal fun <T: Any> RequestResult<T?>.requireData(): T = checkNotNull(data)
+
+internal fun <I, O> RequestResult<I>.map(mapper: (I) -> O): RequestResult<O> {
+    val outData = mapper(data)
+    return when(this) {
+        is RequestResult.Success -> RequestResult.Success(outData)
+        is RequestResult.Error -> RequestResult.Error(outData)
+        is RequestResult.InProgress -> RequestResult.InProgress(outData)
+    }
+}
