002e77d584661ba26ffd759018ab2b62d22be2c6
kura1aym <mukhtarkuralay249@mail.ru>
2024-03-15 17:33:09 +0500

    add Mapper

--- news-data/src/main/java/dev/androidbroadcast/news/data/Mappers.kt
+++ news-data/src/main/java/dev/androidbroadcast/news/data/Mappers.kt
@@ -2,7 +2,13 @@ package dev.androidbroadcast.news.data
 
 import dev.androidbroadcast.news.data.model.Article
 import dev.androidbroadcast.news.database.models.ArticleDBO
+import dev.androidbroadcast.newsapi.modules.ArticleDTO
 
 internal fun ArticleDBO.toArticle() : Article {
     TODO("Not implemented")
 }
+
+internal fun ArticleDTO.toArticleDbo(): ArticleDBO{
+    TODO("Not yet implemented")
+}
+
