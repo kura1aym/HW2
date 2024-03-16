bec2ed976d503b5e7594861fc2742186834ba972
kura1aym <mukhtarkuralay249@mail.ru>
2024-03-16 06:07:21 +0500

    RequestResult map to State

--- features/news-main/src/main/java/dev/androidbroadcast/news/main/NewsMainViewModel.kt
+++ features/news-main/src/main/java/dev/androidbroadcast/news/main/NewsMainViewModel.kt
@@ -1,19 +1,43 @@
 package dev.androidbroadcast.news.main
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.lifecycle.viewModelScope
+import dev.androidbroadcast.news.data.ArticlesRepository
+import dev.androidbroadcast.news.data.RequestResult
+import dev.androidbroadcast.news.data.map
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
-internal class NewsMainViewModel:ViewModel() {
-    private val _state = MutableStateFlow(State.None)
-    val state: StateFlow<State>
-        get() = _state.asStateFlow()
+internal class NewsMainViewModel(
+    private val repository: ArticlesRepository
+):ViewModel() {
+    val state: StateFlow<State> = repository.getAll()
+        .map { requestResult->
+            requestResult.map { articles ->
+                articles.map { it.toUiArticle() }
+            }
+        }
+        .map { it.toState() }
+        .stateIn(viewModelScope, SharingStarted.Lazily, State.None)
+}
+
+private fun dev.androidbroadcast.news.data.model.Article.toUiArticle(): Article {
+    TODO("Not implemented")
+}
+
+private fun RequestResult<List<Article>>.toState(): State {
+    return when(this) {
+        is RequestResult.Error -> State.Error()
+        is RequestResult.InProgress -> State.Loading(data)
+        is RequestResult.Success -> State.Success(checkNotNull(data))
+    }
 }
 
 sealed class State {
     object None : State()
-    class Loading : State()
+    class Loading(val articles: List<Article>?) : State()
     class Error : State()
     class Success(val articles: List<Article>) : State()
 }
