10ff1fc12a22ad465304b0fd3407883ba2f8b9a3
kura1aym <mukhtarkuralay249@mail.ru>
2024-03-16 12:35:58 +0500

    add Application Module

---
+++ app/src/main/java/dev/androidbroadcast/newssearchapp/AppModule.kt
@@ -0,0 +1,31 @@
+package dev.androidbroadcast.newssearchapp
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dev.androidbroadcast.news.data.ArticlesRepository
+import dev.androidbroadcast.news.database.NewsDatabase
+import dev.androidbroadcast.newsapi.NewsApi
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideNewsApi(): NewsApi {
+        return NewsApi(
+           baseUrl = BuildConfig.NEWS_API_BASE_URL,
+           apikey = BuildConfig.NEWS_API_KEY
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsdatabase(@ApplicationContext context: Context): NewsDatabase {
+        return NewsDatabase(context)
+    }
+}
