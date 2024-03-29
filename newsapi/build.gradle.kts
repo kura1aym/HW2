87b8878d4662b94a9d2ffd8888e6dcb19b2901c0
kura1aym <mukhtarkuralay249@mail.ru>
2024-03-29 21:55:30 +0500

    logger interceptor

--- newsapi/build.gradle.kts
+++ newsapi/build.gradle.kts
@@ -13,10 +13,11 @@ java {
 
 dependencies{
     implementation(libs.retrofit)
-    implementation(libs.retrofit.adapters.result)
-    implementation(libs.retrofit.converter.kotlinx.serialization)
     implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.kotlinx.serialization.json)
+    api(libs.kotlinx.serialization.json)
     implementation(libs.androidx.annotation)
+    implementation(libs.retrofit.converter.kotlinx.serialization)
+    implementation(libs.retrofit.adapters.result)
+    api(libs.okhttp)
 }
 
